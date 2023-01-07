7a4e4cd4929c5d935a0ad17c068d397dbaf766c3
Arpit <arpit27kr@gmail.com>
2023-01-07 12:27:11 +0530

    Add files via upload

---
+++ User Registration/src/main/java/io/aadeesh/UserRegistration1Application.java
@@ -0,0 +1,15 @@
+package io.aadeesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class UserRegistration1Application extends SpringBootServletInitializer
+{
+	public static void main(String Args[]) 
+	{
+		SpringApplication.run(UserRegistration1Application.class, Args);
+	}
+	
+}
